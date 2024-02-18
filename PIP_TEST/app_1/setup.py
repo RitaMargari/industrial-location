4ceb84c9ed1ce90a6edbe076d434e8a659433455
George <george.kontsevik@gmail.com>
2024-02-18 22:03:47 +0300

    first commit

---
+++ PIP_TEST/app_1/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+from setuptools import setup
+
+setup(
+    name='lib1',
+    version='0.0.1',
+    packages=['lib1'],
+    install_requires=[
+        # List your dependencies here if any
+    ],
+)
